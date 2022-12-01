4682b4d8eea9d10f550791c4d4cfb5ccd94d15f1
Josh Jones <ohblonddev@gmail.com>
2022-12-01 09:28:08 +0000

    Complete day 1 part 2

--- days/day_1/src/lib.rs
+++ days/day_1/src/lib.rs
@@ -6,15 +6,22 @@ pub fn max_calories_carried(calories: Vec<Vec<usize>>) -> usize {
         .unwrap()
 }
 
+pub fn total_calories_carried_by_top_three_elves(calories: Vec<Vec<usize>>) -> usize {
+    let mut sums: Vec<usize> = calories.into_iter().map(|elf| elf.iter().sum()).collect();
+    sums.sort();
+    sums.reverse();
+    sums.iter().take(3).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use testing_utils::speculoos::assert_that;
     use testing_utils::{data_sample_string, LineGroups};
 
-    use crate::max_calories_carried;
+    use super::*;
 
     #[test]
-    fn max_calories_carried_by_an_elf() {
+    fn part_1() {
         let input: Vec<Vec<usize>> = vec![
             vec![1000, 2000, 3000],
             vec![4000],
@@ -27,7 +34,7 @@ mod tests {
     }
 
     #[test]
-    fn max_calories_carried_by_an_elf_full() {
+    fn part_1_full() {
         let input: Vec<Vec<usize>> = data_sample_string("day_1.txt")
             .line_groups()
             .iter()
@@ -35,5 +42,29 @@ mod tests {
             .collect();
 
         assert_that(&max_calories_carried(input)).is_equal_to(69528);
+    }
+
+    #[test]
+    fn part_2() {
+        let input: Vec<Vec<usize>> = vec![
+            vec![1000, 2000, 3000],
+            vec![4000],
+            vec![5000, 6000],
+            vec![7000, 8000, 9000],
+            vec![10000],
+        ];
+
+        assert_that(&total_calories_carried_by_top_three_elves(input)).is_equal_to(45000);
+    }
+
+    #[test]
+    fn part_2_full() {
+        let input: Vec<Vec<usize>> = data_sample_string("day_1.txt")
+            .line_groups()
+            .iter()
+            .map(|elf| elf.iter().map(|line| line.parse().unwrap()).collect())
+            .collect();
+
+        assert_that(&total_calories_carried_by_top_three_elves(input)).is_equal_to(206152);
     }
 }
