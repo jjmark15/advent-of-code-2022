0ed9289a42a60ef0a8fe5eaba559ee28895f0bcb
Josh Jones <ohblonddev@gmail.com>
2022-12-01 09:33:23 +0000

    Minor refactor

--- days/day_1/src/lib.rs
+++ days/day_1/src/lib.rs
@@ -20,51 +20,43 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn part_1() {
-        let input: Vec<Vec<usize>> = vec![
+    fn short_data_set() -> Vec<Vec<usize>> {
+        vec![
             vec![1000, 2000, 3000],
             vec![4000],
             vec![5000, 6000],
             vec![7000, 8000, 9000],
             vec![10000],
-        ];
-
-        assert_that(&max_calories_carried(input)).is_equal_to(24000);
+        ]
     }
 
-    #[test]
-    fn part_1_full() {
-        let input: Vec<Vec<usize>> = data_sample_string("day_1.txt")
+    fn long_data_set() -> Vec<Vec<usize>> {
+        data_sample_string("day_1.txt")
             .line_groups()
             .iter()
             .map(|elf| elf.iter().map(|line| line.parse().unwrap()).collect())
-            .collect();
-
-        assert_that(&max_calories_carried(input)).is_equal_to(69528);
+            .collect()
     }
 
     #[test]
-    fn part_2() {
-        let input: Vec<Vec<usize>> = vec![
-            vec![1000, 2000, 3000],
-            vec![4000],
-            vec![5000, 6000],
-            vec![7000, 8000, 9000],
-            vec![10000],
-        ];
-
-        assert_that(&total_calories_carried_by_top_three_elves(input)).is_equal_to(45000);
+    fn part_1_short() {
+        assert_that(&max_calories_carried(short_data_set())).is_equal_to(24000);
     }
 
     #[test]
-    fn part_2_full() {
-        let input: Vec<Vec<usize>> = data_sample_string("day_1.txt")
-            .line_groups()
-            .iter()
-            .map(|elf| elf.iter().map(|line| line.parse().unwrap()).collect())
-            .collect();
+    fn part_1_long() {
+        assert_that(&max_calories_carried(long_data_set())).is_equal_to(69528);
+    }
 
-        assert_that(&total_calories_carried_by_top_three_elves(input)).is_equal_to(206152);
+    #[test]
+    fn part_2_short() {
+        assert_that(&total_calories_carried_by_top_three_elves(short_data_set()))
+            .is_equal_to(45000);
+    }
+
+    #[test]
+    fn part_2_long() {
+        assert_that(&total_calories_carried_by_top_three_elves(long_data_set()))
+            .is_equal_to(206152);
     }
 }
