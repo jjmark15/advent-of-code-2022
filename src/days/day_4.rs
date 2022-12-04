c88965f2581165eec7c84039c7f062e0b1a3d600
Josh Jones <ohblonddev@gmail.com>
2022-12-04 14:14:17 +0000

    Complete day 4 part 2

--- src/days/day_4.rs
+++ src/days/day_4.rs
@@ -6,12 +6,27 @@ pub fn part_1(pairs: Vec<Pair>) -> usize {
         .count()
 }
 
+pub fn part_2(pairs: Vec<Pair>) -> usize {
+    pairs
+        .into_iter()
+        .map(|pair| pair.0.overlaps(&pair.1))
+        .filter(|res| *res)
+        .count()
+}
+
 #[derive(Debug)]
 pub struct Assignment(pub usize, pub usize);
 
 impl Assignment {
     fn contains(&self, other: &Self) -> bool {
         self.0 <= other.0 && self.1 >= other.1
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        (self.0 <= other.1 && self.0 >= other.0)
+            || (self.1 >= other.0 && self.1 <= other.1)
+            || (self.0 <= other.0 && self.1 >= other.1)
+            || (self.0 >= other.0 && self.1 <= other.1)
     }
 }
 
@@ -65,5 +80,15 @@ mod tests {
     #[test]
     fn part_1_long() {
         assert_that(&part_1(long_data_set())).is_equal_to(490)
+    }
+
+    #[test]
+    fn part_2_short() {
+        assert_that(&part_2(short_data_set())).is_equal_to(4)
+    }
+
+    #[test]
+    fn part_2_long() {
+        assert_that(&part_2(long_data_set())).is_equal_to(921)
     }
 }
