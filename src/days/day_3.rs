40d21ea4e16a0fdfbd0d45ecb05c80ceb3993cf3
Josh Jones <ohblonddev@gmail.com>
2022-12-04 13:22:19 +0000

    Complete day 3 part 2

--- src/days/day_3.rs
+++ src/days/day_3.rs
@@ -1,15 +1,23 @@
 use std::collections::HashSet;
+use std::hash::Hash;
 
-pub fn part_1(backpacks: Vec<(Vec<char>, Vec<char>)>) -> usize {
+pub fn part_1(backpacks: Vec<Backpack>) -> usize {
     backpacks
         .into_iter()
         .map(|(first, second)| priority(common_item(first, second)))
         .sum()
 }
 
+pub fn part_2(backpacks: Vec<Backpack>) -> usize {
+    grouped(backpacks)
+        .into_iter()
+        .map(|group| priority(identify_badge(group)))
+        .sum()
+}
+
 fn common_item(first: Vec<char>, second: Vec<char>) -> char {
-    let first_set = first.into_iter().collect::<HashSet<char>>();
-    let second_set = second.into_iter().collect::<HashSet<char>>();
+    let first_set = to_set(first);
+    let second_set = to_set(second);
     let intersection = first_set.intersection(&second_set);
     *intersection.min().unwrap()
 }
@@ -21,6 +29,50 @@ fn priority(c: char) -> usize {
         _ => unreachable!(),
     }
 }
+
+fn grouped(backpacks: Vec<Backpack>) -> Vec<Vec<Backpack>> {
+    backpacks
+        .into_iter()
+        .enumerate()
+        .fold(vec![], |mut acc, (index, backpack)| {
+            if index % 3 == 0 {
+                acc.push(vec![backpack])
+            } else {
+                acc.last_mut().unwrap().push(backpack)
+            }
+
+            acc
+        })
+}
+
+fn concatenate((first, second): Backpack) -> Vec<char> {
+    first.into_iter().chain(second).collect()
+}
+
+fn identify_badge(group: Vec<Backpack>) -> char {
+    let sets = group
+        .into_iter()
+        .map(concatenate)
+        .map(to_set)
+        .collect::<Vec<HashSet<char>>>();
+
+    *sets
+        .first()
+        .unwrap()
+        .intersection(sets.get(1).unwrap())
+        .into_iter()
+        .cloned()
+        .collect::<HashSet<char>>()
+        .intersection(sets.get(2).unwrap())
+        .min()
+        .unwrap()
+}
+
+fn to_set<C: IntoIterator<Item = T>, T: Eq + Hash>(v: C) -> HashSet<T> {
+    v.into_iter().collect()
+}
+
+type Backpack = (Vec<char>, Vec<char>);
 
 #[cfg(test)]
 mod tests {
@@ -77,5 +129,15 @@ mod tests {
     #[test]
     fn part_1_long() {
         assert_that(&part_1(long_data_set())).is_equal_to(7826)
+    }
+
+    #[test]
+    fn part_2_short() {
+        assert_that(&part_2(short_data_set())).is_equal_to(70)
+    }
+
+    #[test]
+    fn part_2_long() {
+        assert_that(&part_2(long_data_set())).is_equal_to(2577)
     }
 }
