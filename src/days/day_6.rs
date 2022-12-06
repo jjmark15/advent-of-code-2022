156d60434e8d0f9af62c6ec80872638d19d2ce42
Josh Jones <ohblonddev@gmail.com>
2022-12-06 16:50:13 +0000

    Complete day 6 part 2

--- src/days/day_6.rs
+++ src/days/day_6.rs
@@ -1,22 +1,29 @@
-use std::collections::HashSet;
-
 use itertools::Itertools;
 
 pub fn part_1(signal: String) -> usize {
-    get_index_of_marker(signal.as_str())
+    index_at_end_of_distinct_window(signal.as_str(), 4)
 }
 
-fn get_index_of_marker(signal: &str) -> usize {
-    signal
-        .chars()
-        .tuple_windows()
-        .find_position(|(s1, s2, s3, s4)| {
-            let set: HashSet<&char> = HashSet::from_iter([s1, s2, s3, s4].into_iter());
-            set.len() == 4
+pub fn part_2(signal: String) -> usize {
+    index_at_end_of_distinct_window(signal.as_str(), 14)
+}
+
+fn index_at_end_of_distinct_window(signal: &str, length: usize) -> usize {
+    let chars: Vec<char> = signal.chars().into_iter().collect();
+    chars
+        .iter()
+        .enumerate()
+        .find_position(|(index, _)| {
+            (*index..index + length)
+                .into_iter()
+                .filter_map(|window_iter| chars.get(window_iter))
+                .duplicates()
+                .count()
+                == 0
         })
         .unwrap()
         .0
-        + 4
+        + length
 }
 
 #[cfg(test)]
@@ -51,5 +58,15 @@ mod tests {
     #[test]
     fn part_1_long() {
         assert_that(&part_1(long_data_set())).is_equal_to(1848)
+    }
+
+    #[test]
+    fn part_2_short() {
+        assert_that(&part_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_string())).is_equal_to(19);
+    }
+
+    #[test]
+    fn part_2_long() {
+        assert_that(&part_2(long_data_set())).is_equal_to(2308)
     }
 }
